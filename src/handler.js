3c3aaf809454ad492b2e8e83330c344f0e2b5d2d
pandjihm <pandjihm@yahoo.com>
2021-05-28 15:30:32 +0700

    update handler

---
+++ src/handler.js
@@ -0,0 +1,20 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+
+  const newBook = {
+    name, year, author, summary, publisher, pageCount, readPage, reading, id, insertedAt, updatedAt,
+  };
+
+  books.push(newBook);
+};
+
+module.exports = { addBookHandler };
