245a60a01763590d72f8ff6b3e9c69c61ae99d6f
pandjihm <pandjihm@yahoo.com>
2021-05-28 16:41:21 +0700

    update method: PUT

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,30 @@
+const { addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler } = require('./handler');
+
 const routes = [
   {
     method: 'POST',
     path: '/books',
-    handler: () => {},
+    handler: addBookHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editBookByIdHandler,
   },
 ];
 
